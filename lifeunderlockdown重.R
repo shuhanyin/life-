c4ccba409582f0135013f2c918127eb72424b947
shuhanyin <1243546098@qq.com>
2025-02-28 23:13:25 +0000

    create two summarised tables with mean number of false beliefs for xxx

--- lifeunderlockdown重.R
+++ lifeunderlockdown重.R
@@ -8,3 +8,15 @@ df <- read_csv("life-under-lockdown-wave2-extract.csv")
 spec(df)
 print(df)
 lul2_d <- read_csv("life-under-lockdown-wave2-extract.csv")
+
+lul2_d %>% 
+  mutate(know_facebook = ordered(
+    know_facebook,
+    levels = c(
+      'Nothing at all' ,
+      'Not very much' ,
+      'A fair amount' ,
+      'A great deal'
+    ))) %>%
+  group_by(know_facebook) %>% 
+  summarise(n=n(), beliefs = mean (beliefs))
